887bffd5d4d1a359cacc88640eef082bedfa323a
Rami-Aldahdooh <100088788+Rami-Aldahdooh@users.noreply.github.com>
2022-03-02 11:25:03 +0200

    02-03-2022

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$name = "ali";
+$dept = "IT";
+$avg = 99.4;
+?>
+
+<html>
+<body>
+<table border="1">
+    <tr>
+        <th>name</th>
+        <th>Department</th>
+        <th>Average</th>
+    </tr>
+    <tr>
+        <td><?php echo $name?></td>
+        <td><?php echo $dept?></td>
+        <td><?php echo $avg?></td>
+    </tr>
+</table>
+
+</body>
+</html>
